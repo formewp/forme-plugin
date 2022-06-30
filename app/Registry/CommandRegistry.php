4a3121383ea06e73176010a45f2c66405d58728c
Moussa Clarke <moussaclarke@gmail.com>
2022-06-30 14:06:00 +0100

    chore: job queue command now fully in core

--- app/Registry/CommandRegistry.php
+++ app/Registry/CommandRegistry.php
@@ -9,12 +9,12 @@ declare(strict_types=1);
 namespace VendorName\ReplaceMePlugin\Registry;
 
 use Forme\Framework\Registry\RegistryInterface;
-use WP_CLI;
+//use WP_CLI;
 
 final class CommandRegistry implements RegistryInterface
 {
     public function register(): void
     {
-        WP_CLI::add_command('forme-queue', 'Forme\\Framework\\Commands\\JobQueueCommand');
+        // Register your commands here.
     }
 }
